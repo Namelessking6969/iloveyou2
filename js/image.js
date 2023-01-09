dbcbfedc5529cd577df891bd95d7516ea7b0c866
Namelessking6969 <112624206+Namelessking6969@users.noreply.github.com>
2023-01-09 17:24:12 -0500

    remade index
    Deleted the body of index, re-created javascript

--- js/image.js
+++ js/image.js
@@ -5,5 +5,26 @@ function addPicture(src) {
   }
   
   addPicture("Pictures/20200919_133638.jpg");
-  addPicture("image2.jpg");
-  addPicture("image3.jpg");
+  addPicture("Pictures/20200928_125312.jpg");
+  addPicture("Pictures/20200928_125317.jpg");
+  addPicture("Pictures/20210203_214526.jpg");
+  addPicture("Pictures/20210516_170301.jpg");
+  addPicture("Pictures/20210516_170305.jpg");
+  addPicture("Pictures/20220101_153553");
+  addPicture("Pictures/Alone.jpg");
+  addPicture("Pictures/FB_IMG_1664900855785.jpg");
+  addPicture("Pictures/FB_IMG_1664900880231.jpg");
+  addPicture("Pictures/FB_IMG_1664900885648.jpg");
+  addPicture("Pictures/FB_IMG_1664900891331.jpg");
+  addPicture("Pictures/FB_IMG_1664900894113.jpg");
+  addPicture("Pictures/FB_IMG_1664900900161.jpg");
+  addPicture("Pictures/FB_IMG_1664900903463.jpg");
+  addPicture("Pictures/jrandmywife.jpg");
+  addPicture("Pictures/KissingForehead.jpg");
+  addPicture("Pictures/mywifeandkids.jpg");
+  addPicture("Pictures/OliHandUp.jpg");
+  addPicture("Picutres/Selfiewithbasket.jpg");
+  addPicture("Picutres/WeddingFamily.jpg");
+  addPicture("Picutres/WithAuntNEssa.jpg");
+  addPicture("Picutres/WithJim.jpg");
+
