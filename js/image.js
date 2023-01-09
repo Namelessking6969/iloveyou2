bfecb38263490b1c45456f0f0f510cc5052a4965
Namelessking6969 <112624206+Namelessking6969@users.noreply.github.com>
2023-01-08 19:56:46 -0500

    Create image.js

---
+++ js/image.js
@@ -0,0 +1,9 @@
+function addPicture(src) {
+    var img = document.createElement("img");
+    img.src = src;
+    document.body.appendChild(img);
+  }
+  
+  addPicture("image1.jpg");
+  addPicture("image2.jpg");
+  addPicture("image3.jpg");
